ca34c6660b1110a09ca695c1c1eaafa1e873fe6d
yuneda <alfajryuneda@gmail.com>
2022-08-21 13:33:42 +0700

    [Add] Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+border-bottom: 1px solid #e0e0e0;
+`
+
+const Logo = styled.div`
+font-size: 1.75rem;
+font-weight: 600;
+color: #1c2e65;
+`
+
+const Search = styled.div`
+padding: 5px 10px;
+border-radius: 5px;
+border: 1px solid #e0e0e0;
+display: flex;
+align-items: center;
+width: 300px;
+`
+const FormInput = styled.form`
+width: 100%;
+display: flex;
+align-items: center;
+`
+
+const Input = styled.input`
+width: 100%;
+border: 1px solid transparent;
+&:focus {
+  outline: none;
+}
+`
+
+const Button = styled.button`
+border: none;
+background: white;
+cursor: pointer;
+`
+
+const Creator = styled.div`
+font-size: 1rem;
+font-weight: 400;
+color: #1c2e65;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Logo>My Notes App</Logo>
+      <Search>
+        <FormInput>
+          <Input type="text" placeholder="Search ..." />
+          <Button type="submit">
+            🔎
+          </Button>
+        </FormInput>
+      </Search>
+      <Creator>
+        Achmad Yuneda Alfajr
+      </Creator>
+    </Container>
+  )
+}
+
+export default Navbar
